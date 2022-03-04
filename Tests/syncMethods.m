7787f7065e5bc24b771464a0f433207e0ee5bff8
rulopelon <48516780+rulopelon@users.noreply.github.com>
2022-03-04 19:53:25 +0100

    new methods implemented

--- Tests/syncMethods.m
+++ Tests/syncMethods.m
@@ -1,44 +1,39 @@
 clear,clc,close all force;
 load("variables.mat","symbol_length","CARRIERS","NFFT","PREFIX")
+prefix_length = NFFT*PREFIX;
 
 
 [a,b] = OFDMModV2(10);
 
 n = 0:1:length(b)-1;
-shift =50/length(b);    
-b = b.*exp(-1i*shift*n);
-% signal_noise_shift = [zeros(100,1);signal_noise_shift.'];
+shift =30/length(b);    
+b = b.*exp(1i*shift*n);
 
 b = b.';
- b = [zeros(500,1);b];
+b = [zeros(5000,1);b];
+disp("Expected maximum at 14216 (9216+5000)")
 
-y = [b; zeros(8192,1)].*conj([zeros(8192,1); b]);
+y = [b; zeros(NFFT,1)].*conj([zeros(NFFT,1); b]);
 % Suma
-z = conv(y.',ones(256,1));
+z = conv(y.',ones(prefix_length,1));
 figure
 plot(1:length(z),abs(z))
 
 %% 4.4.1
 [indexes, pilots] = getContinuousPilots();
 m = zeros(8192,1);
-
 for i = indexes
    m(i+(NFFT-CARRIERS-1)/2,1) = pilots(i+1);
 end        
 pilots = ifft(ifftshift(m));
 
-z_2 = zeros(length(b)+length(pilots),1);
-duration = length(b)-length(pilots);
-
-for delay = 0:1:duration
-    y  = conj(b(delay+1:delay+length(pilots))).*pilots;
-    z_2(delay+1) = sum(y);
-end
+z_2 = conv(conj(b),flip(pilots));
 
 figure
 plot(abs(z_2))
+title("Pilots in time domain")
 %% 4.4.2
-b = [zeros(100,1);b];
+
 [indexes, pilots] = getContinuousPilots();
 m = zeros(8192,1);
 
@@ -47,18 +42,20 @@ for i = indexes
 end        
 pilots = ifft(ifftshift(m));
 
-r =[zeros(8192,1);b];
-r_2 = [b;zeros(8192,1)];
-b_2 = conj(r).*r_2;
-
-z_3 = zeros(length(r)+length(pilots),1);
-duration = length(b)-length(pilots)-1;
-
-for delay = 0:1:duration
-     y  = conj(r(delay+1:delay+length(pilots))).*pilots;
-     z_3(delay+1) = sum(y);
-end
-
+r =[zeros(NFFT,1);b];
+r_2 = [b;zeros(NFFT,1)];
+b_2 = conj(r+r_2);
+pilots = flip(pilots);
+pilots_shortened = pilots(1:prefix_length);
+z_3 = abs(conv(b_2,pilots));
+z_4 = abs(conv(b_2,pilots_shortened));
 
 figure
-plot(abs(z_3))
+subplot(2,1,1)
+plot(z_3)
+title("Length of pilots: 8192")
+
+subplot(2,1,2)
+plot(z_4)
+title("Length of pilots: 256")
+
