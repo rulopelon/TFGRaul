128c6e07bfd9a03f66b5050d7886e7f06b5575d1
rulopelon <48516780+rulopelon@users.noreply.github.com>
2022-03-17 15:09:06 +0100

    fixes

--- Reciever.m
+++ Reciever.m
@@ -1,7 +1,7 @@
 function  Reciever(data)
 % Reciever with channel equalization
 load("variables.mat", ...
-    "PREFIX","NFFT","CARRIERS","symbol_length","L","M")
+    "PREFIX","NFFT","CARRIERS","L","M","F")
 
 % Data is resampled to match Fs = 9.14 Mhz
 data_interpolated = interpolation(data,L);
@@ -13,31 +13,10 @@ data_filtered = conv(data_interpolated,reconstruction_filter_reciever,'same');
 data_resampled = data_filtered(1:M:length(data_filtered));
 prefix_length = PREFIX*NFFT;
 % Coarse frequency synchronization
-%coarse_corrected_signal = coarseFrequencySynchronization(data_resampled);
 
 %Frame synchronism
 % Getting the indexes with the start of each symbol
-indexes_synchro = symbolSynchronization(data_resampled);
-initial_index = indexes_synchro(1);
-N_symbols = length(indexes_synchro);
-
-artificial_indexes = zeros(1,N_symbols);
-for i =0:1:N_symbols-1
-    artificial_indexes(i+1) =(NFFT+prefix_length)*i+initial_index;
-end
-% Number of symbols recieved
-disp(N_symbols)
-frame_synchronized = zeros(NFFT+prefix_length,N_symbols);
-
-i = 1;
-for index= artificial_indexes
-    frame_synchronized(:,i) = data_resampled(index-NFFT-prefix_length+1:index).';
-    i = i+1;
-end
-
-frame_synchronized = frame_synchronized(prefix_length+1:end,:);
-% Frequency correction
-frame_synchronized= fineFrequencySynchronization(frame_synchronized);
+frame_synchronized = symbolSynchronization(data_resampled);
 
 % Symbol equalization
 % Symbols are equalized independently
@@ -72,8 +51,12 @@ for i = 1:1:symbols
     % Calculating the correction
     frequency_correction = 1./channel_estimation_interpolated;
     %Substituting inf values with zeros
-    frequency_correction(end-(NFFT-CARRIERS-1)/2 +1:end) = 0;
-    frequency_correction(1:(NFFT-CARRIERS-1)/2) =0;
+    inf_indexes = find(isinf(frequency_correction));
+    for index= inf_indexes
+        frequency_correction(index)=0;
+    end
+    %frequency_correction(end-(NFFT-CARRIERS-1)/2 +1:end) = 0;
+    %frequency_correction(1:(NFFT-CARRIERS-1)/2) =0;
 
     %i_interpolated = ifft(ifftshift(frequency_response));
     % The symbol is equalized
@@ -99,10 +82,14 @@ surveillance_signal = surveillance_signal(:);
 
 % Calculation of the range and doppler of the signal is performed
 
-caf_matrix = BatchProcessing(reference_signal,surveillance_signal);
+[caf_matrix,doppler_axis] = BatchProcessing(reference_signal,surveillance_signal);
+
 [doppler_columns,time_indexes] = max(caf_matrix);
 [~,doppler_index]= max(doppler_columns);
 time_index = time_indexes(doppler_index);
+doppler_axis = linspace(-0.5*Fs_analysis,0.5*Fs_analysis,512);
+doppler_frequency = doppler_axis(doppler_index);
+
 a = 1;
 
 end
