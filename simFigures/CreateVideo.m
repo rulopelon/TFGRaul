a0c023f11e43a1d09221d77043d00b9e599e1d72
rulopelon <201801914@alu.comillas.edu>
2022-07-06 18:06:18 +0200

    Update CreateVideo.m

--- simFigures/CreateVideo.m
+++ simFigures/CreateVideo.m
@@ -1,14 +1,14 @@
 %% Code for generating a video from a secuence of images
 clc,clear
-imageNames = dir(fullfile('sceneryFigures','*.jpg'));
+imageNames = dir(fullfile('2DImages','*.jpg'));
 
 imageNames = {imageNames.name}';
-outputVideo = VideoWriter(fullfile('sceneryFigures','sceneryVideo'));
+outputVideo = VideoWriter(fullfile('2DImages','2Dvideo'));
 outputVideo.FrameRate = 20;
 listNames = natsort(imageNames);
 open(outputVideo)
 for ii = 1:length(listNames)
-   img = imread(fullfile('sceneryFigures',listNames{ii}));
+   img = imread(fullfile('2DImages',listNames{ii}));
    writeVideo(outputVideo,img)
 end
 
